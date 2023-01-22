b65308c0a6f2bdbe1a7e14defd11ae60588ee711
Midhanya-real <57554583+Midhanya-real@users.noreply.github.com>
2023-01-22 15:01:36 +0400

    create migrations

---
+++ database/migrations/2023_01_22_092818_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('country', 20);
+            $table->string('city', 20);
+            $table->integer('house');
+            $table->integer('apartment');
+            $table->integer('postcode');
+            $table->foreignId('user_id')->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
